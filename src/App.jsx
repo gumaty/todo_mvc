e5fb834dbc8e10c07be93d689b8d8f4cbf2a27fb
Arkadiusz Matlak <piccolo@poczta.fm>
2023-05-29 15:13:59 +0200

    task change status

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,77 @@
 import {useState} from "react";
+import "./App.css";
 
 function App() {
 
-  const [value, setValue] = useState('');
-  const [tasks, setTasks] = useState([]);
+    const [value, setValue] = useState('');
+    const [tasks, setTasks] = useState([]);
 
-  function getId(){
-    if(!tasks.length) return 1;
+    function getId() {
+        if (!tasks.length) return 1;
 
-    return Math.max(...tasks.map((task) => task.id)) +1;
-  }
-
-  function handleInput(event){
-    setValue(event.target.value);
-  }
-
-  function handleAddTask(event){
-    if (event.key === "Enter"){
-      setTasks([...tasks, {
-        id: getId(),
-        name: value,
-        status: 'active'
-      }]);
-      setValue('');
+        return Math.max(...tasks.map((task) => task.id)) + 1;
     }
-  }
 
-  function handleDelete(event) {
+    function handleInput(event) {
+        setValue(event.target.value);
+    }
 
-  }
-
-  return (
-    <>
-      <h1>todos</h1>
-      <input
-          type="text"
-          value={value}
-          onChange={handleInput}
-          onKeyUp={handleAddTask}
-      />
-      <ul>
-        {
-          tasks.map((task) => (
-              <li key={task.id}>
-                <span>{task.name}</span>
-                <button
-                  onClick={handleDelete}
-                >delete</button>
-              </li>
-          ))
+    function handleAddTask(event) {
+        if (event.key === "Enter") {
+            setTasks([...tasks, {
+                id: getId(),
+                name: value,
+                status: 'active'
+            }]);
+            setValue('');
         }
-      </ul>
+    }
+
+    function handleDelete(taskToDelete) {
+        return () => {
+            const newTasks = tasks.filter((task) => task !== taskToDelete);
+
+            setTasks(newTasks);
+        }
+    }
+
+
+    function handleChangeStatus(task) {
+        return function () {
+            task.status = task.status === 'active' ? 'done' : 'active';
+            setTasks([...tasks]);
+        }
+    }
+
+
+return (
+    <>
+        <h1>todos</h1>
+        <input
+            type="text"
+            value={value}
+            onChange={handleInput}
+            onKeyUp={handleAddTask}
+        />
+        <ul>
+            {
+                tasks.map((task) => (
+                    <li key={task.id}>
+                        <button
+                            onClick={handleChangeStatus(task)}
+                            className={task.status === 'active' ? 'active' : 'done'}
+                        >{task.status}</button>
+                        <span>{task.name}</span>
+                        <button
+                            onClick={handleDelete(task)}
+                        >delete
+                        </button>
+                    </li>
+                ))
+            }
+        </ul>
     </>
-  )
+)
 }
 
 export default App
